35bd9d253d4bd90c26eef5b9c551db66307a7a75
Yaroslav Hoian <sgoyan1405@gmail.com>
2025-07-28 19:36:23 +0300

    create wish list window

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,1 +1,35 @@
 //Логіка сторінки Wishlist
+import {
+  hadnleAddCart,
+  hadnleAddWishlist,
+  handleClickProducts,
+  handleCloseModal,
+} from './js/handlers';
+import { sumCountCarts, sumCountWishList } from './js/helpers';
+import { getProductsListId } from './js/products-api';
+import refs from './js/refs';
+import { renderProducts } from './js/render-function';
+import { getWishList, initStorage, initStorageWishList } from './js/storage';
+
+initStorage();
+initStorageWishList();
+sumCountCarts();
+sumCountWishList();
+
+let data = getWishList();
+
+async function wishlistPage() {
+  const arrData = data.map(el => getProductsListId(el));
+  const response = await Promise.all(arrData);
+  renderProducts(response);
+}
+
+wishlistPage();
+
+refs.productsList.addEventListener('click', handleClickProducts);
+
+refs.modalCloseBtn.addEventListener('click', handleCloseModal);
+
+refs.addCartBtn.addEventListener('click', hadnleAddCart);
+
+refs.addWishListBtn.addEventListener('click', hadnleAddWishlist);
